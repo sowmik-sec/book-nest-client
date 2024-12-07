d2a8a88187808b91eea6dbec02a39930b4b2a543
sowmik-sec <sowmik.sec@gmail.com>
2024-12-07 19:16:34 +0600

    add route

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import Signin from "../Pages/auth/Signin";
+import Signup from "../Pages/auth/Signup";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <Signin />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
       },
     ],
   },
