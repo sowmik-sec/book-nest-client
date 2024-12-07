9307bd496deeaaa380dd221fe89235ff7db1a2ed
sowmik-sec <sowmik.sec@gmail.com>
2024-12-07 19:15:41 +0600

    create navbar

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar";
 
 export default function Main() {
   return (
     <>
-      <h2 className="text-4xl">Navbar</h2>
+      <Navbar />
       <Outlet />
       <h3 className="text-2xl">Footer</h3>
     </>
